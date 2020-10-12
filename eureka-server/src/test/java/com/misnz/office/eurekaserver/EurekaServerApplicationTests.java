689ebd73c48e6240067adeef860a798a08177eac
chenyejun <chenyejun@beyondbit.com>
2020-10-12 21:35:07 +0800

    eurka-server/gateway

---
+++ eureka-server/src/test/java/com/misnz/office/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misnz.office.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
