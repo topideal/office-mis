32f38d124a227c827e46cac4f46d0282f7488248
git <svuc982cyj@163.com>
2021-11-28 10:51:20 +0800

    加入gateway-routes

---
+++ gateway-routes/src/main/java/com/misnz/office/gateway/routes/GatewayRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.misnz.office.gateway.routes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayRoutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayRoutesApplication.class, args);
+    }
+
+}
---
+++ gateway-routes/src/test/java/com/misnz/office/gateway/routes/GatewayRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misnz.office.gateway.routes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayRoutesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
